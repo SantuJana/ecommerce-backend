d5e7eb09d11e24c39676106d10a41fcf6a70076b
santu jana <santu.bluehorse@gmail.com>
2024-02-09 23:10:48 +0530

    category

--- src/models/productCategory.model.js
+++ src/models/productCategory.model.js
@@ -8,17 +8,26 @@ const productCategorySchema = new mongoose.Schema({
     },
     slug: {
         type: String,
-        required: true,
         unique: true,
     },
     image: {
         type: String,
         required: false,
     },
+    sequence: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive'],
+        required: true,
+        default: 'active',
+    },
 }, { timestamps: true, })
 
-productCategorySchema.pre('save', function ( category ){
-    category.slug = category.name.replace(' ', '-').toLowerCase();
+productCategorySchema.pre('save', async function (  ){
+    this.slug = this.name.replace(' ', '-').toLowerCase();
 })
 productCategorySchema.pre(['updateOne', 'findOneAndUpdate'], function ( category ){
     category.slug = category.name.replace(' ', '-').toLowerCase();
--- src/routes/admin/product.route.js
+++ src/routes/admin/product.route.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { list } = require('../../controllers/admin/product.controller');
+const { list, categoryList, addEditCategory, deleteCategory, toggleCategoryStatus } = require('../../controllers/admin/product.controller');
 
 const router = express.Router();
 
 router.get('', list);
+router.get('/category', categoryList);
+router.post('/category', addEditCategory);
+router.post('/category/delete', deleteCategory);
+router.get("/category/toggle-status/:id", toggleCategoryStatus)
 
 module.exports = router;
